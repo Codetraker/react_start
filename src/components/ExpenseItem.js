f7ef154c6ee2d8df55e5b2dc07254a1494154e94
Codetraker <swarajbibhuprasad@gmail.com>
2023-12-29 13:21:03 +0530

    Add expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,12 @@
 function ExpenseItem(){
-    return <h2>Expense Item</h2>
+    return (
+        <div>
+                <h2>Expense Items</h2>
+                <div>Food Rs 10</div>
+                <div>Petrol Rs 100</div>
+                <div>Movies Rs 200</div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
