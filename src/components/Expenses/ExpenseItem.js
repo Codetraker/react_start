7fc54509248ad45e3b6670b934aceede2181cbc3
Codetraker <swarajbibhuprasad@gmail.com>
2023-12-31 13:15:53 +0530

    Arranging file

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import Card from '../Ui/Card';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetail';
 import './ExpenseItem.css';
@@ -5,10 +6,10 @@ import './ExpenseItem.css';
 function ExpenseItem(props){
     
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <ExpenseDetail amount={props.amount} title={props.title} location={props.location} />
-        </div>
+        </Card>
     );
 }
 
