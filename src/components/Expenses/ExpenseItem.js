22a07504edc76bd793422cd3a31106db20d8d702
Codetraker <swarajbibhuprasad@gmail.com>
2024-01-03 16:42:05 +0530

    Completly change same as instructor

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,19 @@
-import Card from '../Ui/Card';
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetail from './ExpenseDetail';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props){
-    
-    return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <ExpenseDetail amount={props.amount} title={props.title} location={props.location} />
-        </Card>
-    );
+const ExpenseItem = (props) => {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
