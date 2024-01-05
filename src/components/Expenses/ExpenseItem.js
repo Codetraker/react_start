ce5693b2ff251775738d802193ca65df051b158a
Codetraker <swarajbibhuprasad@gmail.com>
2024-01-05 11:59:18 +0530

    Add expense using useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,19 +5,19 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title,setTitle] = useState(props.title);
+  const [amount,setAmount] = useState(props.amount);
   const clickHandler = () =>{
-      setTitle('Updated');
-      console.log(title);
+      setAmount(100);
+      console.log(amount);
   };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${amount}</div>
       </div>
-      <button onClick={clickHandler}>Delete Expenses</button>
+      <button onClick={clickHandler}>Add Expense</button>
     </Card>
   );
 }
