344773501e1dfa459491b9437d86e24ae7eee361
Codetraker <swarajbibhuprasad@gmail.com>
2024-01-10 01:31:28 +0530

    done all task for chapter 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
   {
@@ -28,11 +28,12 @@ const DUMMY_EXPENSES = [
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = (expense) =>{
-    setExpenses(preExpenses => {
-      return [expense, ...preExpenses];
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
     });
   };
+
   // return React.createElement(
   //   'div',
   //   {},
@@ -42,10 +43,10 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [amount,setAmount] = useState(props.amount);
-  const clickHandler = () =>{
-      setAmount(100);
-      console.log(amount);
-  };
   return (
-  <li>
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${amount}</div>
-      </div>
-      <button onClick={clickHandler}>Add Expense</button>
-    </Card>
-  </li>  
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
